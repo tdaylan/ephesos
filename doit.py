108c3512271d432574d72b6176957343b1cba974
Tansu Daylan <tansu.daylan@gmail.com>
2019-02-22 08:49:17 -0500

    Initial commit

---
+++ doit.py
@@ -0,0 +1,11 @@
+import astroquery
+import allesfitter
+import os, sys
+from astroquery.mast import Observations
+from tdpy.util import read_tess, summgene
+import numpy as np
+
+tici = int(sys.argv[1])
+strgfold = sys.argv[2]
+tesstarg.main.init_alle(tici, strgfold)
+
