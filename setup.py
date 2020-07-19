1d90dc9d1ab13754f791681e7a17a5d7ff5844f4
Tansu Daylan <tansu.daylan@gmail.com>
2020-07-19 12:21:47 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'tesstarg',
+    packages = find_packages(),
+    version = '1.0',
+    description = 'Library of useful routines in exoplanet science',
+    author = 'Tansu Daylan',
+    author_email = 'tansu.daylan@gmail.com',
+    url = 'https://github.com/tdaylan/tesstarg',
+    download_url = 'https://github.com/tdaylan/tesstarg', 
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python'],
+    #install_requires=['astrophy>=3'],
+    include_package_data = True
+    )
+
